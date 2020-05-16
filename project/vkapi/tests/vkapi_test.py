e50a4bb517da2f36a34bd8ac9767db8eb4e53581
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2020-05-17 02:39:39 +0300

    project done vk api

---
+++ project/vkapi/tests/vkapi_test.py
@@ -0,0 +1,16 @@
+
+import vkapi
+import pytest
+
+import sys
+
+@pytest.mark.parametrize('shortname', [
+    '',
+    '123321', # состоит только из цифр. Это запрещено, потому что тогда в апишке вк нельзя будет различить, мы передали id или передали короткое имя
+    'mrsndmn 1231', # содержит пробел
+    'абвгд', # содержит кириллицу
+    'mrsndmn,durov', # содержит запятую
+])
+def test_bad_shortnames(shortname):
+    # print(vkapi.get_user_id_by_shortname)
+    assert vkapi.get_user_id_by_shortname(shortname) == {}
---
+++ project/vkapi/vkapi.py
@@ -0,0 +1,47 @@
+from flask import Flask
+
+import vk_api
+import os
+import re
+
+from dotenv import load_dotenv
+load_dotenv()
+
+VK_API_TOKEN = os.getenv("VK_API_TOKEN")
+if VK_API_TOKEN == "":
+    raise ValueError("VK_API_TOKEN env variable is required")
+
+vk_session = vk_api.VkApi(token=VK_API_TOKEN)
+
+print(vk_session.method('users.get', values={"user_ids": "mrsndmn"}))
+
+
+app = Flask(__name__)
+
+host = '127.0.0.1'
+port = 5000
+
+@app.route('/vk_id/<shortname>')
+def get_user_id_by_shortname(shortname: str):
+    if re.match(r'^\d+$', shortname):
+        return {}
+
+    # в id должны быть только буквы, цифры и "_"
+    if not re.match(r'^[a-zA-Z0-9_]+$', shortname):
+        return {}
+
+    try:
+        res = vk_session.method('users.get', values={"user_ids": "mrsndmn"})
+
+        if isinstance(res, list) and len(res) > 0:
+            return {"vk_id": res[0]['id']}
+
+        return
+    except Exception as e:
+        print("Can't get user id by shortname:", e)
+
+    return {}
+
+
+if __name__ == '__main__':
+    app.run(host=host, port=port)

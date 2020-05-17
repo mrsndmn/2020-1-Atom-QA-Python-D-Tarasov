5e6857c8dd7a1ebf2fd77b45f3449a0024b3e8ce
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2020-05-17 15:04:30 +0300

    project wip users model

--- project/model/myapp_user.py
+++ project/model/myapp_user.py
@@ -1,6 +1,13 @@
 
+import sqlalchemy
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy import Column, Integer, String, Date, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 
 Base = declarative_base()
 
@@ -27,4 +34,33 @@ class User(Base):
                 f"start_active_time='{self.start_active_time}'" \
                f")>"
 
+class MysqlOrmConnection:
 
+    def __init__(self, user, password, db_name):
+        self.user = user
+        self.password = password
+        self.db_name = db_name
+
+        self.host = os.getenv('MYSQL_HOST', '127.0.0.1')
+        self.port = os.getenv('MYSQL_PORT', 3306)
+
+        self.connection = self.get_connection()
+
+        session = sessionmaker(bind=self.connection.engine)
+        self.session = session()
+
+    def get_connection(self, db_created=False):
+        engine = sqlalchemy.create_engine(
+            'mysql+pymysql://{user}:{password}@{host}:{port}/{db}'.format(user=self.user,
+                                                                          password=self.password,
+                                                                          host=self.host,
+                                                                          port=self.port,
+                                                                          db=self.db_name if db_created else ''),
+            encoding='utf8'
+        )
+
+        return engine.connect()
+
+    def execute_query(self, query):
+        res = self.connection.execute(query)
+        return res.getchall()

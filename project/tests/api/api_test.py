8b31134d4cb4f834fa1ac377eeb593da2e17d647
Dmitry Tarasov <d.tarasov@corp.mail.ru>
2020-05-24 14:45:53 +0300

    project duplicate tests

--- project/tests/api/api_test.py
+++ project/tests/api/api_test.py
@@ -6,9 +6,6 @@ from model.myapp_user import User
 from myapp.client import MyAppClient
 
 # длинное пользовательское имя
-# Длинный пароль
-# длинный емейл
-# логин null
 # неуникальность емайла, логина
 # null в базе там, где, возможно, это не предусмотрели
 # обновление последнего захода пользвоателя
@@ -20,18 +17,19 @@ fake = faker.Faker()
 
 
 @pytest.mark.api
-class TestAPI:
-
+class TestAPIBase:
     @pytest.fixture(scope='function', autouse=True)
     def setup(self, request, logger):
         self.myapp_client: MyAppClient = request.getfixturevalue('api_client')
         self.myapp_client.logger = logger
 
 
+class TestAPICreateUser(TestAPIBase):
+
     @pytest.mark.parametrize('username, password, email', [
         (fake.lexify('?'*16), fake.lexify('?'*255), fake.lexify('?'*52) + '@example.com'),  # граничный случай
     ])
-    def test_create_user_positive_boundary(self, username, password, email):
+    def test_positive_boundary(self, username, password, email):
         '''
         Логин, пароль и почта максимально допустимой длинны
         '''
@@ -43,7 +41,7 @@ class TestAPI:
         ('еёюя', 'Кириллица запрещена'),
         ('1', 'Слишком короткий пароль'),
     ])
-    def test_create_user_bad_username(self, username, badpassword, email, desc):
+    def test_bad_username(self, username, badpassword, email, desc):
         '''
         Негативные кейсы по созданию пользователей.
         '''
@@ -56,7 +54,7 @@ class TestAPI:
         ('ёёёёёёеёюя', 'Кириллица запрещена'),
         ('1', 'Слишком короткий логин'),
     ])
-    def test_create_user_bad_username(self, badusername, password, email, desc):
+    def test_bad_username(self, badusername, password, email, desc):
         '''
         Негативные кейсы по созданию пользователей.
         '''
@@ -68,14 +66,14 @@ class TestAPI:
         ('@', 'Невалидный email'),
         (fake.lexify(text='?'*53)+'@example.com' , 'Слишком длинная почта'),
     ])
-    def test_create_user_bad_email(self, username, password, bademail, desc):
+    def test_bad_email(self, username, password, bademail, desc):
         '''
         Кейсы с невалидой почтой
         '''
         resp = self.myapp_client.add_user(username, password, bademail)
         assert resp.status_code == 400, desc
 
-    def test_create_user_duplicate(self, username, password, email):
+    def test_duplicate_full(self, username, password, email):
         """
         При повторном создании пользователя должна возвращаться ошибка 304, already exists
         """
@@ -83,7 +81,27 @@ class TestAPI:
         resp = self.myapp_client.add_user(username, password, email)
         assert resp.status_code == 304
 
-    def test_create_user_unauthorized(self, username, password, email):
+    def test_duplicate_username(self, username, password, email):
+        """
+        При создании пользователя с одинаковым username, должа возвращаться 304
+        """
+        resp = self.myapp_client.add_user(username, password, email)
+
+        username = fake.user_name()
+        resp = self.myapp_client.add_user(username, password, email)
+        assert resp.status_code == 304
+
+    def test_duplicate_email(self, username, password, email):
+        """
+        При создании пользователя с одинаковым email, должа возвращаться 304
+        """
+        resp = self.myapp_client.add_user(username, password, email)
+
+        email = fake.ascii_email()
+        resp = self.myapp_client.add_user(username, password, email)
+        assert resp.status_code == 304
+
+    def test_unauthorized(self, username, password, email):
 
         old_session = self.myapp_client.req_session
         self.myapp_client.req_session = requests.Session()
